dc21057e2bd97ac8c25ac0941f14727a3b56619d
Rubin Peci <rubinpeci@gmail.com>
2018-09-13 22:07:10 -0400

    Done!

--- 03_occupation/occupations.py
+++ 03_occupation/occupations.py
@@ -3,7 +3,7 @@
 #Period 7
 #2018-09-13
 
-from random import randint, choices
+from random import choices
 
 def parse_data(filename):
     file = open(filename, 'r')  #open the file in read mode
@@ -20,14 +20,32 @@ def parse_data(filename):
         list[counter] = list[counter].rsplit(',', 1)
         counter += 1
     
+    #make the empty dictionary and loop thru
+    #the raw data of the file
     dict = {}
     counter = 1
-    while counter < len(list) - 1:
+    while counter < len(list) - 2:
         dict[list[counter][0]] = float(list[counter][1])
         counter += 1
-    print(dict)
+
+    #print(dict) #diagnostic print statement
     return dict
 
 def pick_job(dict):
-    return
-parse_data('occupations.csv')
+    jobs = list(dict.keys()) #list of keys
+    values = []              #empty list to be filled with values
+
+    #loop thru the dict to get the weights/values
+    for key in jobs:
+        values.append(dict[key])
+
+    #diagnostic print statements
+    #print(values)
+    #print(jobs)
+
+    #uses the choices method in random which allows for one to pick from a list of items
+    #and have it be weighted
+    job = choices(jobs, values)
+    return job[0]
+
+print(pick_job( parse_data('occupations.csv') ))
