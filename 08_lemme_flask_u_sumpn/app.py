f15fa311e5338e4ca3d7999565d0fa6a1c4d368d
Rubin Peci <rubinpeci@gmail.com>
2018-09-19 20:59:16 -0400

    Add HW#08

---
+++ 08_lemme_flask_u_sumpn/app.py
@@ -0,0 +1,21 @@
+#Rubin Peci
+#Period 7
+#Software Development
+#2018-09-19
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def first_page():
+    return 'Hi this is the very first page of my server!'
+
+@app.route('/ligma')
+def second_page():
+    return "What's ligma?"
+
+@app.route('/ASTROWORLD')
+def third_page():
+    return "THIRTEEN HOURS TILL I LAND"
+
