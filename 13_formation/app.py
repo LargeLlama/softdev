8cb7ea252576c1230269197a97ed48f05a79ab69
Rubin Peci <rubinpeci@gmail.com>
2018-09-28 15:54:27 -0400

    Fix 13

--- 13_formation/app.py
+++ 13_formation/app.py
@@ -1,18 +1,30 @@
 from flask import Flask, render_template, request
-
+from random import randint
 app = Flask(__name__)
 
 @app.route("/")
 def test():
-	return render_template("test.html")
+	# generates a random number between 1-100 and gives a 50/50 chance between making the type post or get
+	option = randint(1, 101)
+	if option <= 50:
+		return render_template("home.html", choice="POST")
+	else:
+		method = "GET"
+		return render_template("home.html", choice="GET")
 
 @app.route("/auth", methods=['GET', 'POST'])
 def authorize():
+	''' Diagnostic statements
 	print(app)
 	print(request)
 	print(request.args)
-	print(request.headerst)
-	return render_template("form.html", firstname=request.args['firstname'], lastname=request.args['lastname'], request_type=request.method)
+	print(request.headers)	
+	'''
+	#Will do the proper method type based of whatever requests says it is
+	if request.method == "POST":
+		return render_template("form.html", firstname=request.form['firstname'], lastname=request.form['lastname'], request_type=request.method)
+	else:
+		return render_template("form.html", firstname=request.args['firstname'], lastname=request.args['lastname'], request_type=request.method)
 
 app.debug = True
 if __name__ == "__main__":
