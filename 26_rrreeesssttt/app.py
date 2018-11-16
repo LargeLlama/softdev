20603f547418284e6b4d65f301fde78f5ff2ef0e
Rubin Peci <rubinpeci@gmail.com>
2018-11-15 23:03:10 -0500

    wu

--- 26_rrreeesssttt/app.py
+++ 26_rrreeesssttt/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session, request
 import urllib, json
 from util import words
 
@@ -6,5 +6,6 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    temp = words.get_definition('lacrosse')
-    return render_template("home.html", definition = temp)
+    temp_word = request.args.get('search', 'lacrosse') 
+    temp = words.get_definition(temp_word)
+    return render_template("home.html", word = temp_word, definition = temp)
